7e833f7caae2d710cd496c458e3fc22e6b0b56c9
Meng <alexmeng@70cd606e6c04.ant.amazon.com>
2012-05-17 16:45:41 -0700

    Update script to install correctly

--- installDotFiles.sh
+++ installDotFiles.sh
@@ -1,31 +1,31 @@
-#! /bin/zsh
+#! /bin/sh
+##
+# The script installs the dotfiles for the user and vim plugins
+## 
+
+# bufexplorer (see http://www.vim.org/scripts/script.php?script_id=42)
+plugins[0]=http://www.vim.org/scripts/download_script.php\?src_id\=14208
+# color sample pack (see http://www.vim.org/scripts/script.php?script_id=625)
+plugins[1]=http://www.vim.org/scripts/download_script.php?src_id=12179 
+# NERDTree (see http://www.vim.org/scripts/script.php?script_id=1658)
+plugins[2]=http://www.vim.org/scripts/download_script.php?src_id=17123
 
 # Install oh-my-zsh (see https://github.com/robbyrussell/oh-my-zsh)
-curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+curl -sL https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
 # Install pathogen (see https://github.com/tpope/vim-pathogen)
 mkdir -p ~/.vim/autoload ~/.vim/bundle; \
 curl -so ~/.vim/autoload/pathogen.vim \
     https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
-# Install bufexplorer (see http://www.vim.org/scripts/script.php?script_id=42)
-BUF='bufExplorer.zip'
-curl -so $BUF http://www.vim.org/scripts/download_script.php\?src_id\=14208
-unzip $BUF -d ~/.vim/bundle
-rm -f $BUF
-
-# Install color sample pack (see http://www.vim.org/scripts/script.php?script_id=625)
-COLORPACK='ColorSamplerPack'
-curl -so $COLORPACK http://www.vim.org/scripts/download_script.php?src_id=12179 
-unzip $COLORPACK ~/.vim/bundle
-rm -f $BUF
-
-# Install NERDTree
-NERDTREE='nerdtree.zip'
-curl -so $NERDTree http://www.vim.org/scripts/download_script.php?src_id=17123
-unzip $NERDTree -d ~/.vim/bundle
-rm -f $NERDTree
-
+# Install VIM plugins
+for plugin in ${plugins[@]} 
+do
+	TMP='tmp.zip'
+	curl -o $TMP $plugin
+	unzip -f $TMP -d ~/.vim/bundle
+	rm -f $TMP
+done
 
 # Copy over zshrc config files
 cp .zshrc ~/
